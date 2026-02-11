dc9ff7d72a022ba2ff6a4e9585ae2adb3378d478
NIRANJAN-PATIL-1811 <rosh.rp1811@gmail.com>
2026-02-11 16:17:14 +0530

    added index.js

--- index.js
+++ index.js
@@ -3,10 +3,4 @@ const express = require('express');
 
 const app = express();
 
-
-app.listen(3000, "0.0.0.0", () => {
-  console.log('Server is running at http://0.0.0.0:3000');
-});
-
-
 module.exports = app;
